d1fe5244c1fa62cbabc2b8cb87b9a31eb9516c50
Sabrina196 <sabrinapatri96@gmail.com>
2023-05-05 14:48:25 -0300

    1-Primer Portfolio: Mayo 2023

---
+++ README.md
@@ -0,0 +1,12 @@
+# Portfolio
+
+Te enseño mi primer portfolio, por el que todavía sigo trabajando a medida que voy realizando o mejorando algunos de mis principales proyectos.
+
+El mismo esta realizado con:
+- HTML5
+- CSS3
+- Bootstrap v5.1.1
+- Iconos de Bootstrap
+- Javascript
+- Fuente de Googles  
+	. [Google Fonts](https://fonts.google.com/knowledge "Google Fonts")
---
+++ script.js
@@ -0,0 +1,43 @@
+//Variables
+let btnAcademico = document.getElementById("academico");
+let btnIntereses = document.getElementById("intereses");
+let btnHabilidades = document.getElementById("habilidades");
+let caja1= document.querySelector(".caja1");
+let caja2= document.querySelector(".caja2");
+let caja3= document.querySelector(".caja3");
+
+
+//Evento Click de los botones del div Contenedor "Acerca de Mi"
+btnAcademico.addEventListener("click", mostrarDiv);
+btnIntereses.addEventListener("click", mostrarDiv);
+btnHabilidades.addEventListener("click", mostrarDiv);
+
+
+//Función para mostrar y ocultar las secciones del Contenedor "Acerca de Mi"
+function mostrarDiv(){
+    let seleccionado= this.value;
+    if(seleccionado == 1)
+    {
+        caja1.style.display="block";
+        caja2.style.display="none";
+        caja3.style.display="none";
+
+    }
+    else if(seleccionado == 2)
+    {
+        caja2.style.display="block";
+        caja1.style.display="none";
+        caja3.style.display="none";
+    }
+    else
+    {
+        caja3.style.display="block";
+        caja1.style.display="none";
+        caja2.style.display="none";
+    }
+    
+}
+
+
+
+
